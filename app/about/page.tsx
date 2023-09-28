55aac7051bf5a01d6ee29771b12ca8f008921902
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-29 02:39:41 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,10 +10,7 @@ export default function About() {
 					I am a passionate software engineer from Egypt.
 				</p>
 
-				
-
 				<p className="lg:text-2xl md:text-lg xxs:text-sm mb-3 leading-loose">
-
 					After Graduating with a Degree in Accounting I decided to
 					pursue my passion for programming, I Enrolled in 3 months
 					FWD programme and after that I Enrolled in the 15 months ALX
@@ -30,22 +27,20 @@ export default function About() {
 				</p>
 			</div>
 			<div className="mt-6">
-
 				<h2 className="lg:text-3xl md:text-lg xxs:text-sm mb-10">
 					you can download my CV here
 				</h2>
 				<div className="text-center flex justify-center xxs:mb-[35px] xs:mb-[35px]">
-=======
-				<h2 className="lg:text-3xl md:text-lg xxs:text-sm mb-10">you can download my CV here</h2>
-				<div className="text-center xxs:mb-[35px] xs:mb-[35px]">
->>>>>>> 3103abd9bc666ae917c8139ffbbd32ed63799c9b					<a
-						href="/Ahmed-AbdElHameed-CV.pdf"
-						download
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-center items-center gap-2 w-[12rem] group transition-all  "
-					>
-						Download CV
-						<HiDownload className="text-sm opacity-70 transition-all group-hover:translate-y-1" />
-					</a>
+					<div className="text-center xxs:mb-[35px] xs:mb-[35px]">
+						<a
+							href="/Ahmed-AbdElHameed-CV.pdf"
+							download
+							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-center items-center gap-2 w-[12rem] group transition-all  "
+						>
+							Download CV
+							<HiDownload className="text-sm opacity-70 transition-all group-hover:translate-y-1" />
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
